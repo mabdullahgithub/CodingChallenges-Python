297344be39f6adbceae3ad4051316bce35f6a423
Muhammad Abdullah <abdullahorg15@gmail.com>
2023-09-23 10:53:39 -0700

    create afile

---
+++ type conversion function.py
@@ -0,0 +1,8 @@
+#here we are going to learn about type conversion function
+
+# int() function
+print(type(3.14)) #float
+print(type(int(3.14))) #it will convert float to int
+
+print(type("3131")) #string
+print(type(int("3131"))) #it will convert string to int
